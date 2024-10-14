478c24ed60c54270821e35b48c321c2a05fabfda
Abdul Rafay <abdulrafayn001@gmail.com>
2024-10-14 12:59:25 -0700

    Refactor ProtectedRouteProvider to handle auth checks client-side and prevent flicker

--- providers/ProtectedRouteProvider.tsx
+++ providers/ProtectedRouteProvider.tsx
@@ -2,7 +2,7 @@
 
 import { isAuthenticated } from "@/lib/utils/auth";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ProtectedRouteProvider = ({
   children,
@@ -11,15 +11,25 @@ const ProtectedRouteProvider = ({
 }) => {
   const router = useRouter();
   const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const isAuthPage = pathname === "/login" || pathname === "/signup";
-    if (!isAuthenticated() && !isAuthPage) {
-      router.push("/signin");
-    } else if (isAuthenticated() && isAuthPage) {
-      router.push("/profile");
-    }
+    const checkAuth = () => {
+      const isAuthPage = pathname === "/login" || pathname === "/signup";
+      if (!isAuthenticated() && !isAuthPage) {
+        router.push("/signin");
+      } else if (isAuthenticated() && isAuthPage) {
+        router.push("/profile");
+      }
+      setIsLoading(false);
+    };
+
+    checkAuth();
   }, [router, pathname]);
+
+  if (isLoading) {
+    return null; // or a loading spinner
+  }
 
   if (!isAuthenticated() && pathname !== "/login" && pathname !== "/signup") {
     return null;
